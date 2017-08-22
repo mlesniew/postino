9ff0ad606bd0be406e3d027e997a64e79b098be3
Michał Leśniewski <mlesniew@gmail.com>
2017-08-22 20:54:47 +0200

    Basic implementation

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='postino',
+    version='0.2',
+    description='Easy email sending',
+    url='http://github.com/mlesniew/postino',
+    author='Michał Leśniewski',
+    author_email='mlesniew@gmail.com',
+    license='GPLv3',
+    packages=['postino'],
+    zip_safe=False)
